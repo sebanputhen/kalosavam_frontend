6fb7936c6f00e9c969e8d8c4da4873abf43e5a70
sebanputhen <sebastian.puthenveedu@gmail.com>
2026-08-06 16:54:29 +0530

    jun 700

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -23,7 +23,7 @@ axiosInstance.interceptors.request.use(
   (config) => {
     const token = getAuthToken();
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`; 
     }
     return config;
   },
