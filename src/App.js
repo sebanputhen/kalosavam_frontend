2f008fa1c219c4c74385834c14d2ae3fa1795f5a
sebanputhen <sebastian.puthenveedu@gmail.com>
2026-09-10 14:14:12 +0530

    al5

--- src/App.js
+++ src/App.js
@@ -128,12 +128,11 @@ function App() {
             {isParishUser() && (
               <ParishLayout>
                 <Switch>
-                  { <Route exact path="/parish/registration" component={TransactionPage} />
+                  <Route exact path="/parish/registration" component={TransactionPage} />
                   <Route exact path="/parish/managers" component={FamilyNew} /> 
-                  <Route exact path="/parish/participantList" component={ParticipantList} />
-                
-
-                  <Route exact path="/parish/print" component={ChurchReportPage} />}
+                 
+<Route exact path="/parish/participantList" component={ParticipantList} />
+                  <Route exact path="/parish/print" component={ChurchReportPage} />
                   <Route path="*">
                     <Redirect to="/parish/registration" />
                   </Route>
